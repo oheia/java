16801dd8e0d0f176d1665394f39b1c0319ded36f
oheia <oheia@outlook.com>
2021-02-17 21:13:26 +0800

    java-base --java基础编写样例

---
+++ java-base/src/main/java/com/ohei/javabase/dynamicproxy/DynamicProxy.java
@@ -0,0 +1,31 @@
+package com.ohei.javabase.dynamicproxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+/**
+ * java动态代理demo
+ */
+public class DynamicProxy {
+
+    public static void main(String[] args) {
+        Person person = (Person) Proxy.newProxyInstance(Person.class.getClassLoader(),new Class[] {Person.class},new MyInvocationHandler());
+        person.say("Hello World!");
+    }
+
+}
+
+class MyInvocationHandler implements InvocationHandler{
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if(method.getName().equals("say")){
+            System.out.println("xiaomin say : " + args[0]);
+        }
+        return null;
+    }
+}
+
+interface Person{
+    String say(String content);
+}
